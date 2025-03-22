935bedeb39abd2d1b888f123cb1beac3cdf8d17d
Max Fosh <204212234+maxfoshw95@users.noreply.github.com>
2025-03-23 00:16:56 +0800

    Removed unused file

--- VAR_example.py
+++
@@ -1,7 +0,0 @@
-components = {}
-components_dir = "./components/" # Components directory
-components_suffix = ".components.html" # Suffix for the component
-
-frontend = "./frontend/" # Frontend directory
-build_dir = "./dist/" # Build directory
-
